7caeafa73a3907bd985490bb5a3e722b6cd99045
thanhyk14 <thanhaxt342@gmail.com>
2023-12-12 22:58:09 +0700

    edit

--- routes/web.base.php
+++ routes/web.base.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('web')->group(static function (): void {
     Route::get('/', function () {
-        return ['sound/api' => app()->version()];
+        return ['sound/api: v.1.0.0'];
     });
 });
